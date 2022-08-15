83f1da4045cb6984b6bc6f2ab3b17baa8ef0b873
mathiasdbq <mathiasbandin1@homtial.com>
2022-08-14 22:25:12 -0300

    reacondicionamiento de conflictos

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 let formLogin = document.getElementById('form-login');
 let loginEmail = document.getElementById('email-login');
 let logincontrasena = document.getElementById('contrasena-login')
+let imgLoginEmail = document.getElementById ('login-img-email')
+let imgLoginpassword = document.getElementById ('login-img-password')
 formLogin.addEventListener('submit',function(evento){
 
     let errorEmail = document.getElementById('texto-error-email')
@@ -12,16 +14,19 @@ formLogin.addEventListener('submit',function(evento){
             errorEmail.style.display = 'block' ;
             errorEmail.innerHTML = 'ingresa tu e-mail' ;
             loginEmail.style.border = 'red solid 0.5px';
+            imgLoginEmail.style.display = 'block'
         } else if (contenido == 'contrasena') {
             evento.preventDefault();
             errorContasena.style.display = 'block' ;
             errorContasena.innerHTML = 'ingresa una contraseña' ;
             logincontrasena.style.border = 'red solid 0.5px';
+            imgLoginpassword.style.display = 'block'
         } else if (contenido == 'contrasenaCorta') {
             evento.preventDefault();
             errorContasena.style.display = 'block' ;
             errorContasena.innerHTML = 'ingresa una contraseña mayor a 8 caracteres' ;
             logincontrasena.style.border = 'red solid 0.5px';
+            imgLoginpassword.style.display = 'block'
         }
     };
 
@@ -30,29 +35,34 @@ formLogin.addEventListener('submit',function(evento){
             evento.preventDefault();
             errorEmail.style.display = 'none' ;
             loginEmail.style.border = 'solid 0.5px #CFD4DA';
+            imgLoginEmail.style.display = 'none'
         } else if (contenido == 'contrasena') {
             evento.preventDefault();
             errorContasena.style.display = 'none' ;
             logincontrasena.style.border = 'solid 0.5px #CFD4DA';
+            imgLoginpassword.style.display = 'none'
         }
     }
 
     if (loginEmail.value.length<7 && logincontrasena.value.length<1) {
         error('email')
         error('contrasena');  
-    } else if (loginEmail.value.length<7 && logincontrasena.value.length > 1 && logincontrasena.value.length < 8){
+    }else if (loginEmail.value.length<7 && logincontrasena.value.length >= 1 && logincontrasena.value.length <= 8){
         error('email')
         error('contrasenaCorta')  
-    } else if(logincontrasena.value.length > 8 && loginEmail.value.length<7){
+    }else if(logincontrasena.value.length >= 8 && loginEmail.value.length<7){
         error('email')
         noerror('contrasena')
     }else if(logincontrasena.value.length<1){
         error('contrasena');
         noerror('email')
-    } else if( logincontrasena.value.length > 1 && logincontrasena.value.length < 8){
+    }else if(logincontrasena.value.length<=1){
+        error('contrasenaCorta');
+        noerror('email')
+    }else if( logincontrasena.value.length > 1 && logincontrasena.value.length <= 8){
         error('contrasenaCorta') 
         noerror('email') 
-    } else if (loginEmail.value.length > 7 && logincontrasena.value.length > 8){
+    }else if (loginEmail.value.length >= 7 && logincontrasena.value.length > 8){
         evento.preventDefault();
         window.location.href='./inicio.html';
     }
